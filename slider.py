3231ad97a67a7e1528647cec60a6752bfff93e50
anthony <130861676+trashzito@users.noreply.github.com>
2024-05-13 15:32:31 -0300

    slider.py

---
+++ slider.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.slider import Slider
+class MyApp(App):
+    def build(self):
+        return Slider(min=0, max=100, value=50)
+if __name__ == '__main__':
+    MyApp().run() 
