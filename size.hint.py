a9057d0261417db40d9a399b339393bf75cd5a9b
anthony <130861676+trashzito@users.noreply.github.com>
2024-05-13 15:32:04 -0300

    size.hint.py

---
+++ size.hint.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.button import Button
+class MyApp(App):
+    def build(self):
+        return Button(text='clique aqui!', size_hint=(0.5, 0.2))
+if __name__ == '__main__':
+    MyApp().run() 
